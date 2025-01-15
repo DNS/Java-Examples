075361a342e64461220e54057c56bc6c71dd9241
DNS <37093+DNS@users.noreply.github.com>
2025-01-15 19:10:50 +0700

    -

--- objectoriented.java
+++ objectoriented.java
@@ -1,7 +1,42 @@
 
-public class objectoriented {
-	public static void main (String[] args) {
-		//System.out.println();
-	}
+// Define the Car class
+class ObjectTest {
+    public int X;
+    private int Y;
+
+    // Constructor
+    public ObjectTest (int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+
+
+    public int get_Y () {
+        return this.Y;
+    }
+
+    public void set_Y (int value) {
+        this.Y = value;
+    }
 }
+
+
+
+
+public class objectoriented {
+    public static void main(String[] args) {
+        ObjectTest oo = new ObjectTest(100, 200);
+		
+		oo.X = 888;
+		// oo.Y = 777;	// error: Y has private access
+		oo.set_Y(500);
+		
+        System.out.println("X: " + oo.X);
+        System.out.println("Y: " + oo.get_Y());
+		
+		
+
+    }
+}
+
 

08855ea8b0f39147799989c38bb63753e98a13db
DNS <37093+DNS@users.noreply.github.com>
2025-01-15 15:32:22 +0700

    -

---
+++ variables.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+//import java.util.Map;
+
+public class variables {
+	public static void main (String[] args) {
+		System.out.println("--------");
+		
+		// VARIABLES: Integer, String
+		int x = 123;
+		String s = "Hello World";		
+		System.out.println(x);
+		System.out.println(s);
+		
+		System.out.println("--------");
+		
+		// ARRAY
+		int[] arr = {11, 22, 33};
+		for (int i: arr) {
+			System.out.println(i);
+		}
+		
+		System.out.println("--------");
+		
+		// HASH
+		HashMap<String, Integer> hm = new HashMap<>();
+		hm.put("one", 1);
+		hm.put("two", 2);
+		for (HashMap.Entry<String, Integer> i: hm.entrySet()) {
+			System.out.println(i.getKey() + ": " + i.getValue());
+		}
+		
+		System.out.println("--------");
+		
+		
+	}	
+}
+
+
+

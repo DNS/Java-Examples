e086b4a0bc0d4e114bed34c1f170fe068b2ed568
DNS <37093+DNS@users.noreply.github.com>
2025-01-15 16:30:10 +0700

    A: stderr.java

---
+++ fileread.java
@@ -0,0 +1,6 @@
+public class fileread {
+	public static void main (String[] args) {
+		//System.out.println();
+	}
+	
+}
---
+++ stderr.java
@@ -0,0 +1,10 @@
+
+public class stderr {
+	public static void main (String[] args) {
+		System.err.println("Error msg!");
+	}
+	
+}
+
+
+

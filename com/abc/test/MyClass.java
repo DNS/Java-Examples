def0465d174b5f3fc5506abc3e952cea2700c4c3
DNS <37093+DNS@users.noreply.github.com>
2025-01-16 21:55:15 +0700

    namespace

---
+++ com/abc/test/MyClass.java
@@ -0,0 +1,11 @@
+package com.abc.test;
+
+public class MyClass {
+	public static int x;
+	
+	// constructor
+	public MyClass () {
+		this.x = 777;
+	}
+	
+}
---
+++ namespace.java
@@ -0,0 +1,11 @@
+import com.abc.test.*;
+
+public class namespace {
+	public static void main (String[] args) {
+		MyClass a = new MyClass();
+		System.out.println(a.x);
+		
+	}
+
+}
+
